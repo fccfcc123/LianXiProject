df0bba2b875c1528b9c865cefe1d4672a49870a1
fcc1 <123456@163.com>
2022-10-24 18:13:58 +0800

    forth step : alter a line add 22222!

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello xiaohu!");
-        System.out.println("test1111111");
+        System.out.println("test11111112222");
     }
 }
