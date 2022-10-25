6d515bc81286f4a6178317484d4b190871a65adc
fcc1 <123456@163.com>
2022-10-25 10:46:52 +0800

    second  commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,5 +4,6 @@ public class hello {
         System.out.println("master add a line !");
         System.out.println("master add a line agin");
         System.out.println(2222);
+        System.out.println("3333");
     }
 }
