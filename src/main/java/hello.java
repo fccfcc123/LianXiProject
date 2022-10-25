12b1f4d2b412cf0cf01593cde2eb9d734c2c541d
fccfcc123 <89235331+fccfcc123@users.noreply.github.com>
2022-10-25 10:47:22 +0800

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,5 +4,7 @@ public class hello {
         System.out.println("master add a line !");
         System.out.println("master add a line agin");
         System.out.println(2222);
+        sout
+       
     }
 }
