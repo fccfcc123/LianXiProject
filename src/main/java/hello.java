8daf4e46e55ddaebad23ec19d29ff7cd2639b82d
fcc1 <123456@163.com>
2022-10-25 10:45:31 +0800

    first  commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,6 @@ public class hello {
         System.out.println("test master 1111333333");
         System.out.println("master add a line !");
         System.out.println("master add a line agin");
-        System.out.println(2222)
+        System.out.println(2222);
     }
 }
