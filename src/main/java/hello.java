da92f0ac09a14e0bf680c7fe1969cad3c10a287c
fcc1 <123456@163.com>
2022-10-25 09:58:02 +0800

    8 step : 合并

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("test master");
         System.out.println("master add a line !");
+        System.out.println("branch1 add a line");
     }
 }
