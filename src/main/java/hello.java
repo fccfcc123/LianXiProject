5409d078926f088f950b59a80c3080ea2ba060f9
fcc1 <123456@163.com>
2022-10-25 09:35:31 +0800

    fith： step : alter a line master

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello xiaohu!");
-        System.out.println("test11111112222");
+        System.out.println("test master");
     }
 }
