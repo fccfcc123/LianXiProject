dd43387b73200848458cd1e0173b78bc6c02958a
fccfcc123 <89235331+fccfcc123@users.noreply.github.com>
2022-10-25 10:37:54 +0800

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("test master 1111");
+        System.out.println("test master 1111333333");
         System.out.println("master add a line and  alter something !");
         System.out.println("branch1 add a line");
     }
